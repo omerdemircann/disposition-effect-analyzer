5b94d24d0d8e72bd9a1b70485c371d3a50a71cd2
omer demircan <omerdemircan36@gmail.com>
2026-05-08 10:16:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ bist_tickers = {
     "Turkish Airlines": "THYAO.IS",
     "Erdemir": "EREGL.IS",
     "Tupras": "TUPRS.IS",
-    "Aselsan": "ASELS.IS",
+    "Aselsan": "ASELS.IS"
+    "Isbank": "ISCTR.IS",
 }
 
 with st.sidebar:
